958506570e2609813ae6122b49cfcd22e9b55c41
Anton <9264622898@mail.ru>
2023-03-13 20:23:19 +0300

    код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк в массиве (размерность массива)");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] mas = new string[n];
+            int n_3 = 0;
+            int index = 0;
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine("Введите "+(i+1)+" слово: ");
+                mas[i] = Console.ReadLine();
+                if (mas[i].Length<=3){n_3++;}
+            }
+
+            Console.Write("Введеный массив строк: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mas[i]+" ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
